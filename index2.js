67e670a8d24fe76f3c6d5b6165a863bc1f8dd1f7
livingSMART50 <148806415+livingSMART50@users.noreply.github.com>
2024-07-09 22:21:43 +0100

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,22 @@
+var ctx = document.getElementById('lineChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [{
+            label: 'Milestone',
+            data: [60, 25, 40, 50, 35, 20, 50, 50, 100, 150, 24, 57],
+            backgroundColor: [
+                'rgba(85,85,85, 1)'
+
+            ],
+            borderColor: 'rgb(41, 155, 99)',
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
+
---
+++ project.js
@@ -0,0 +1,13 @@
+//sellect items
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.search projects');
+const submitBtn = document.querySelector('.submit-btn');
+const grocery = document.getElementById('projects');
+const container = document.querySelector('.project-container');
+const list = document.querySelector('.project-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+//edit options
+let editElement;
+let editFlag = false;
+let editID = "";
